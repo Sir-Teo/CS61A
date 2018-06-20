ec17ae77beea67955d9f8b94b3f2158dfdfa84d7
Teo <zengwc.teo2016@outlook.com>
2018-06-20 13:36:58 -0700

    lab01

---
+++ labs/lab01/lab01.py
@@ -0,0 +1,14 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Q3
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    "*** YOUR CODE HERE ***"
